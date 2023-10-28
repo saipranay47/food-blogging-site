c89248e73a132295d5161f9be2ef2d266088fc81
saipranay47 <saipranayadepu07@gmail.com>
2023-10-28 19:16:54 +0530

    fix button text color bug

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ children, variant, additionalClasses, ...props }) => {
-  const baseClasses = "rounded-full py-3 px-7 text-white max-md:text-[11px] max-md:py-2 max-md:px-4";
+  const baseClasses = "rounded-full py-3 px-7  max-md:text-[11px] max-md:py-2 max-md:px-4";
 
   const outlinedClasses =
     "text-black border border-black bg-transparent hover:bg-gray-100 hover:text-gray-800";
